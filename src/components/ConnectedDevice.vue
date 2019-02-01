3cda0460814ca5b6196f4a50e00991c03f52a09d
NGUYENANHTUAN\Dell <des.nguyenanhtuan@gmail.com>
2019-02-01 21:04:24 +0700

    Create BaseDevice, Switch button and Modal info

---
+++ src/components/ConnectedDevice.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Connect to your devices</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseDevice',
+}
+</script>
+
+<style lang="scss" module>
+@import url('../design/index.scss');
+</style>
---
+++ src/components/SwitchButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button>Switch button on/off</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SwitchButton',
+}
+</script>
+
+<style lang="scss" module>
+</style>
